1a2b45a817027049fe23243083a790886eb41425
Matthew Craig <mgcraig78@gmail.com>
2020-08-11 16:44:38 -0700

    refactor CardList

--- src/index.js
+++ src/index.js
@@ -5,15 +5,11 @@ import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
+import CardList from './CardList.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-    </div>
+    <CardList robots={robots}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
